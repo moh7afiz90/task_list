6534a33d73a78ce2885b7054f94a68b3d12d0192
Mohanad Elhag <moh.7afiz@gmail.com>
2018-02-02 17:45:15 +0300

    Added Delete & Filter

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 // Define UI variables
-const form = document.querySelector('#task-form');
-const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.clear-tasks');
-const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#task');
-var $warningEmptyTask = $('<span>Add a task</span>').add($('<button class="btn-flat toast-action">Undo</button>'));
+const form = document.querySelector('#task-form')
+const taskList = document.querySelector('.collection')
+const clearBtn = document.querySelector('.clear-tasks')
+const filter = document.querySelector('#filter')
+const taskInput = document.querySelector('#task')
+var $warningEmptyTask = $('<span>Add a task</span>').add($('<button class="btn-flat toast-action">Undo</button>'))
+var $warningRomoveTask = $('<span>Are You Sure?</span>').add($('<button class="btn-flat toast-action">Undo</button>'))
 
 
 // Load all event listeners
-loadEventListeners();
+loadEventListeners()
 
 // Load all event listeners
 function loadEventListeners() {
   // add task event
-  form.addEventListener('submit', addTask);
+  form.addEventListener('submit', addTask)
+  // Remove task event
+  taskList.addEventListener('click', removeTask)
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks)
+  // Filter Tasks Event
+  filter.addEventListener('keyup', filterTasks)
 }
 
 // Add Task
 function addTask(e) {
   if(taskInput.value === ''){
-    Materialize.toast($warningEmptyTask, 1000);
+    Materialize.toast($warningEmptyTask, 1000)
   }
 
   // Create li element
-  const li = document.createElement('li');
+  const li = document.createElement('li')
   // Add Class
-  li.className = 'collection-item';
+  li.className = 'collection-item'
   // Create text node and append to li
   li.appendChild(document.createTextNode(taskInput.value))
   // Create new link element
@@ -41,8 +48,46 @@ function addTask(e) {
   taskList.appendChild(li)
 
   // clear input
-  taskInput.value = '';
+  taskInput.value = ''
 
 
-  e.preventDefault();
+  e.preventDefault()
+}
+
+// Remove Task
+function removeTask(e){
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure?')){
+      e.target.parentElement.parentElement.remove()
+    }
+  }
+}
+
+// Clear Tasks
+function clearTasks(){
+  // One Way
+  // taskList.innerHTML = '';
+
+  // Another way (Faster way)
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild)
+  }
+
+
+  // Reverence
+  // https://jspref.com/innerHTML-vs-removechild
+}
+
+// Filter Tasks
+function filterTasks(e){
+  const text = e.target.value.toLowerCase()
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block'
+    } else {
+      task.style.display = 'none'
+    }
+  })
+
 }
